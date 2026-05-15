e96041b658fcef6cccf8e6d7c51d43be5b141589
장윤선 <yunseon0308@gmail.com>
2026-05-15 14:27:04 +0900

    feat: add Google Analytics 4 tracking (G-DY80TQLMZL)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 const ADSENSE_CLIENT = process.env.NEXT_PUBLIC_ADSENSE_CLIENT || '';
+const GA_ID = 'G-DY80TQLMZL';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://stay-calendar.vercel.app'),
@@ -51,6 +52,21 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         {modal}
+
+        {/* Google Analytics 4 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+
         {ADSENSE_CLIENT && (
           <Script
             async
